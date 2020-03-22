3a607c139e3c4d73a69f13153b32e544f9b24898
Lahlukap669 <luka1.lah@gmail.com>
2020-03-22 12:29:23 +0100

    Prepared for heroku 1

--- api_flask.py
+++ api_flask.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 import json
 import hashlib
+from flask_cors import CORS
 app = Flask(__name__)
 
+CORS(app)
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
